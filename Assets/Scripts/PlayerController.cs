0b6437bfa6113aa4c7e566b4a0a8c6ff9b655858
Andrey <frost.rus@gmail.com>
2018-04-07 01:51:28 +0300

    99% ready game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, 2 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(-2 * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, -2 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(2 * Time.deltaTime, 0);
+        }
+    }
+}
