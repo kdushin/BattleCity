e2e1671153cd3db9c804052a95fe09540b4adf31
Dekkeret <kdushin1990@gmail.com>
2018-04-07 19:48:52 +0300

    Add damageprovider

---
+++ Assets/Scripts/Damage/DamageProvider.cs
@@ -0,0 +1,13 @@
+﻿using Damage;
+using UnityEngine;
+
+public class DamageProvider : MonoBehaviour
+{
+    [SerializeField] private int damage = 1;
+
+    void OnTriggerEnter(Collider2D colliderHit)
+    {
+        IDestroyable destroyableCollider = colliderHit.GetComponent<IDestroyable>();
+        if (destroyableCollider != null) destroyableCollider.Hit(damage);
+    }
+}
